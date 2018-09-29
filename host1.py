5e1b385f2d9e93875ccfe88293a6b1669c1e0ab4
tkg97 <gupta.gupta.tarun@gmail.com>
2018-09-29 12:52:19 +0530

    UDP code

--- host1.py
+++ host1.py
@@ -9,6 +9,7 @@ import logging
 import pickle
 import select
 import random
+import logging
 from myqueue import Queue
 from collections import namedtuple
 from frame import data_frame, ack_frame
@@ -20,7 +21,7 @@ receive_window = 1
 expected_ack = 0
 expected_pkt = 0
 frame_to_send = 0
-timer_duration = 50000 #in milliseconds
+timer_duration = 1000 #in milliseconds
 
 q = Queue(transmit_window)
 time_stamp_q = Queue(transmit_window)
@@ -34,23 +35,29 @@ def should_drop(threshold) :
 	if(temp<=threshold) : return True
 	else : return False
 
-client_sender = socket.socket()
-while True:
-	try:
-		client_sender.connect(('169.254.224.230', 6654))
-		break
-	except TimeoutError:
-		pass
+client_receiver_address = ('169.254.224.230', 6654)
 
-
-sender_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 ip = '169.254.224.230'
 port = 6653
 address = (ip, port)
-sender_server.bind(address)
-sender_server.listen(1)
-client_receiver, __addr_receiver = sender_server.accept()
+receiver.bind(address)
+receiver.setblocking(0)
+
+logfile_name = "host1.log"
+
+logging.basicConfig(filename=logfile_name, level=logging.DEBUG)
+
 base_time = time.time()*1000
+
+def send(new_packet_string):
+	global receiver
+	while(True) :
+		try:
+			receiver.sendto(new_packet_string, client_receiver_address)
+			return
+		except Exception as __e:
+			pass
 
 def respond_to_pck_resending(resend_pkts):
 	#Resend all the buffered packets
@@ -67,47 +74,42 @@ def pkt_sender(resend_pkts):
 	global transmit_window
 	global frame_to_send
 	global q
-	global client_receiver
 	global max_seq_num
 	while(True):
 		if(resend_pkts.is_set()) : continue
 		cur_time = time.time()*1000 - base_time
-		if(q.full()==False):
+		if(q.full() == False):
 			if (tempq.empty()) :
 				# create and send packet here
 				new_packet = data_frame(frame_to_send % max_seq_num)
-				cur_time = time.time()*1000	- base_time				
+				cur_time = time.time()*1000 - base_time
 
-				print ("generated packet %d, at time %f" %(frame_to_send, cur_time))
+				logging.info ("generated packet %d, at time %f" %(frame_to_send, cur_time))
 
 				new_packet_string = pickle.dumps(new_packet)
 				q.put(new_packet) #Insert a packet into queue
 				time_stamp_q.put(cur_time)
-				__ready = select.select([],[client_receiver],[])
 				if (should_drop(packet_drop_probability) == False) :
-					print ("packet %d, at time %f, sent successfully" %(frame_to_send, cur_time))
-					client_receiver.send(new_packet_string)
+					logging.info ("packet %d, at time %f, sent successfully" %(frame_to_send, cur_time))
+					send(new_packet_string)
 				frame_to_send = (frame_to_send + 1)%max_seq_num
 			else :
 				#Get that pack from tempq top
 				new_packet = tempq.get()
-				cur_time = time.time()*1000 - base_time
-
-				print ("Trying to resend the packet number %d at time %f" %(new_packet.data_num, cur_time))
+				cur_time = time.time()*1000	- base_time				
 				
+				logging.info ("Trying to resend the packet number %d at time %f" %(new_packet.data_num, cur_time))
+
 				new_packet_string = pickle.dumps(new_packet)
 				q.put(new_packet) #Insert a packet into queue
 				time_stamp_q.put(cur_time)
-				__ready = select.select([],[client_receiver],[])
 				if (should_drop(packet_drop_probability) == False) :
-					print ("resent the packet number %d at time %f" %(new_packet.data_num, cur_time))
-					client_receiver.send(new_packet_string)
-			time.sleep(0.0025)
+					logging.info ("resent the packet number %d at time %f" %(new_packet.data_num, cur_time))
+					send(new_packet_string)
+			time.sleep(0.002)
 
 
 def pkt_receiver(resend_pkts):
-	global client_sender
-	global client_receiver
 	global expected_pkt
 	global expected_ack
 	global q
@@ -119,38 +121,38 @@ def pkt_receiver(resend_pkts):
 			if abs(cur_time - time_stamp_q.top()) >= timer_duration :
 				# We should have got the acknowledgement for this packet by now
 				# Means we need to signal the timeout event
-				print ("Timeout occured at time %f, will need to resent all the packets in window" %cur_time)
+				logging.info ("Timeout occured at time %f, will need to resend all the packets in window" %cur_time)
 				resend_pkts.set()
 				respond_to_pck_resending(resend_pkts)
 
-		ready = select.select([client_sender],[],[],0.005)
+		ready = select.select([receiver],[],[],0.005)
 		if(ready[0]):
-			# print ("hey")			
-			data = client_sender.recv(4096)
-			packet = pickle.loads(data)
-			cur_time = time.time()*1000 - base_time
-			if isinstance(packet, data_frame) :
-				if packet.data_num == expected_pkt :
-					print ("received data packet - %d at time %f" %(expected_pkt, cur_time))
-					# create and send the acknowledgement for this received packet
-					ack = ack_frame(expected_pkt)
-					ack_string = pickle.dumps(ack)
-					ready = select.select([],[client_receiver],[])
-					print ("sending ack for data packet - %d at time %f" %(expected_pkt, cur_time))
-					if (should_drop(ack_drop_probability) == False) :
-						print ("Success - ack for data packet - %d at time %f" %(expected_pkt, cur_time))
-						client_receiver.send(ack_string)
-					expected_pkt = (expected_pkt + 1)%max_seq_num
-			elif isinstance(packet, ack_frame) :
-				print ("received ack for data packet - %d at time %f" %(packet.ack_num, cur_time))
-				# print ("**** %d $$$$" %q.size())				
-				while((q.empty()==False) and ((q.top()).data_num != packet.ack_num)) :
-					q.get()
-					time_stamp_q.get()
-				if ((q.empty() == False) and ((q.top()).data_num == packet.ack_num)) :
-					q.get()
-					time_stamp_q.get()
-				# print ("**** %d" %q.size())								
+			try:
+				data, __address = receiver.recvfrom(4096)
+				packet = pickle.loads(data)
+				cur_time = time.time()*1000 - base_time
+				if isinstance(packet, data_frame) :
+					if packet.data_num == expected_pkt :
+						logging.info ("received data packet - %d at time %f" %(expected_pkt, cur_time))
+						# create and send the acknowledgement for this received packet
+						ack = ack_frame(expected_pkt)
+						ack_string = pickle.dumps(ack)
+						logging.info ("sending ack for data packet - %d at time %f" %(expected_pkt, cur_time))
+
+						if (should_drop(ack_drop_probability) == False) :
+							logging.info ("Success - ack for data packet - %d at time %f" %(expected_pkt, cur_time))
+							send(ack_string)
+						expected_pkt = (expected_pkt + 1)%max_seq_num
+				elif isinstance(packet, ack_frame):
+					logging.info ("received ack for data packet - %d at time %f" %(packet.ack_num, cur_time))			
+					while((q.empty()==False) and ((q.top()).data_num != packet.ack_num)) :
+						q.get()
+						time_stamp_q.get()
+					if ((q.empty()==False) and ((q.top()).data_num == packet.ack_num)) :
+						q.get()
+						time_stamp_q.get()
+			except Exception as __e:
+				pass
 
 
 if __name__ == '__main__':
